585b756ecca71ea458012c99c22debd69b31c1db
Carlos Gomez <carlos_gomez@brown.edu>
2015-04-01 16:15:10 -0400

    moved my package from cmg1 to group

--- src/main/java/edu/brown/cs/group/lyricFinder/Song.java
+++ src/main/java/edu/brown/cs/group/lyricFinder/Song.java
@@ -1,5 +1,33 @@
 package edu.brown.cs.group.lyricFinder;
 
-public class Song {
+import java.io.Serializable;
 
+public class Song implements Serializable {
+  private static final long serialVersionUID = 2195261848992500743L; // temporary
+  private int id;
+  private String title;
+  private String artist;
+  private String lyrics;
+
+  public Song(int id, String title, String artist, String lyrics) {
+    this.id = id;
+    this.title = title;
+    this.artist = title;
+  }
+
+  public int getID() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public String getLyrics() {
+    return lyrics;
+  }
 }
--- src/main/java/edu/brown/cs/group/lyricFinder/SongDatabase.java
+++ src/main/java/edu/brown/cs/group/lyricFinder/SongDatabase.java
@@ -1,5 +1,33 @@
 package edu.brown.cs.group.lyricFinder;
 
-public class SongDatabase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class SongDatabase {
+  private Connection conn;
+
+  public SongDatabase(String db) throws ClassNotFoundException {
+    Class.forName("org.sqlite.JDBC");
+    String urlToDB = "jdbc:sqlite:" + db;
+
+    try {
+      this.conn = DriverManager.getConnection(urlToDB);
+    } catch (SQLException e) {
+      // TODO Auto-generated catch block
+      System.out.println(e.getErrorCode());
+      e.printStackTrace();
+    }
+
+    // if database doesn't already exist, build it...
+    // otherwise, you're done
+  }
+
+  public void buildDatabase(String path) {
+    
+  }
+
+  public void connectToDatabase(String path) {
+    
+  }
 }
