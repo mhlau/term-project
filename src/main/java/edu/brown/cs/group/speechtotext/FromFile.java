51f8cf0bc2383739f2c5248e3dd0e0de50f8408d
Gianluca Pane <gianluca_pane@brown.edu>
2015-04-01 16:17:37 -0400

    asdf

--- src/main/java/edu/brown/cs/group/speechtotext/FromFile.java
+++ src/main/java/edu/brown/cs/group/speechtotext/FromFile.java
@@ -1,5 +1,12 @@
 package edu.brown.cs.group.speechtotext;
 
-public class FromFile extends SpeechToText {
+import java.io.IOException;
 
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+public class FromFile extends SpeechToText {
+	
+	public FromFile(String filename) throws IOException{
+		this.recognizer = new StreamSpeechRecognizer(config);
+	}
 }
