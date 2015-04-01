9a7946e3173eb986b4c8a6ff41eed6ccdc33df52
Carlos Gomez <carlos_gomez@brown.edu>
2015-04-01 16:53:27 -0400

    began adding a test, unfinished

--- src/main/java/edu/brown/cs/group/lyricFinder/SongDatabase.java
+++ src/main/java/edu/brown/cs/group/lyricFinder/SongDatabase.java
@@ -1,8 +1,14 @@
 package edu.brown.cs.group.lyricFinder;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class SongDatabase {
   private Connection conn;
@@ -11,6 +17,7 @@ public class SongDatabase {
     Class.forName("org.sqlite.JDBC");
     String urlToDB = "jdbc:sqlite:" + db;
 
+    /*
     try {
       this.conn = DriverManager.getConnection(urlToDB);
     } catch (SQLException e) {
@@ -21,10 +28,26 @@ public class SongDatabase {
 
     // if database doesn't already exist, build it...
     // otherwise, you're done
+     * 
+     */
   }
 
+  // this should prob not take anything and just use the connection...
   public void buildDatabase(String path) {
-    
+    try {
+      for (int id = 1; id <= 5; id++) {
+        Document doc = Jsoup.connect("http://songmeanings.com/songs/view/" + id).get();
+        String artistAndTitle = doc.title();
+        Elements lyrics = doc.body().getElementsByClass("holder lyric-box");
+        for (Element l : lyrics) {
+          String s = l.text();
+          System.out.println(artistAndTitle + " " + s);
+        }
+      }
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   public void connectToDatabase(String path) {
---
+++ src/test/java/edu/brown/cs/group/lyricFinderTests/SongDatabaseTest.java
@@ -0,0 +1,44 @@
+package edu.brown.cs.group.lyricFinderTests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import edu.brown.cs.group.lyricFinder.SongDatabase;
+
+public class SongDatabaseTest {
+  @BeforeClass
+  public static void setUpClass() throws Exception {
+    // (Optional) Code to run before any tests begin goes here.
+  }
+
+  @AfterClass
+  public static void tearDownClass() throws Exception {
+    // (Optional) Code to run after all tests finish goes here.
+  }
+
+  @Before
+  public void setUp() {
+    // (Optional) Code to run before each test case goes here.
+  }
+
+  @After
+  public void tearDown() {
+    // (Optional) Code to run after each test case goes here.
+  }
+
+  @Test
+  public void getActorIDTest() {
+    try {
+      SongDatabase sdb = new SongDatabase("songDB.sqlite3");
+      sdb.buildDatabase("");
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+}
