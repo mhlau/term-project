86e2a3ea1bc4b2b8be85fcc3599d9b86bf6ae88f
Micah Lau <micah_lau@brown.edu>
2015-04-17 13:28:56 -0400

    Adding resources for GUI.

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,20 @@
+var searchButton = document.getElementById("searchButton");
+var searchInput = document.getElementById("searchInput");
+var resultUrlText = document.getElementById("resultUrl");
+
+var search = function() {
+	var searchVal = searchInput.value;
+	var postParams = {
+		"searchVal" : searchVal
+	};
+	$.post("/result", postParams, function(responseJSON) {
+		response = JSON.parse(responseJSON);
+		resultUrl = response.resultUrl;
+		resultUrlText.innerHTML = "<iframe width=\"560\" height=\"315\"" + 
+		"src=\"" + resultUrl.resultUrl + "\""
+		+ " frameborder=\"0\" allowfullscreen></iframe>";
+		console.log(resultUrl);		
+	});
+};
+
+searchButton.onclick = search;
