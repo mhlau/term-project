9dbad4381381e0e503c972fc0e18d0bd5c2c855a
Alexander Levine <alexander_levine@brown.edu>
2015-04-29 04:09:03 -0400

    Added topic and weighted topic/sentiment matching

--- src/main/java/edu/brown/cs/group/term_project/Main.java
+++ src/main/java/edu/brown/cs/group/term_project/Main.java
@@ -5,6 +5,9 @@ import edu.brown.cs.group.matcher.BooleanWordSource;
 import edu.brown.cs.group.matcher.NRCWordSource;
 import edu.brown.cs.group.matcher.SongMatcher;
 import edu.brown.cs.group.matcher.NormalizedVectorDistanceSongMatcher;
+import edu.brown.cs.group.matcher.TopicMatcher;
+import edu.brown.cs.group.matcher.SongSourceDistance;
+import edu.brown.cs.group.matcher.TwoSourceMatcher;
 import edu.brown.cs.group.speechtotext.FromFile;
 import edu.brown.cs.group.speechtotext.LiveMode;
 import edu.brown.cs.group.speechtotext.SpokenWord;
@@ -13,7 +16,8 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 public final class Main {
 
   private Main() {
@@ -22,17 +26,23 @@ public final class Main {
 
   public static void main(String[] args) throws Exception {   //Returns names of top 5 songs
     System.out.println("Main is running.");
-        
-    SongDatabase db = new SongDatabase(args[0], Integer.valueOf(args[2]));
-    BooleanWordSource ws = new NRCWordSource(new File(args[1]));
     
-    List<String> dialogue = new ArrayList<String>();
-    SongMatcher sm = new NormalizedVectorDistanceSongMatcher(ws, db);
+    SongDatabase db = new SongDatabase(args[0], 15);
     
-    if (args.length > 2 && args[2].equals("--speech")){
+   List<String> dialogue = new ArrayList<String>();
+    //SongMatcher sm = new TopicMatcher(new BufferedReader(new FileReader(args[1])), db.getAllSongs());
+    List<Song> allSongs = db.getAllSongs();
+    SongMatcher sm = new TwoSourceMatcher(
+      new SongSourceDistance(
+        new BufferedReader(new FileReader(args[1])) , allSongs),
+      new SongSourceDistance(
+        new BufferedReader(new FileReader(args[2])) , allSongs),
+      allSongs, .8);
+    if (args.length > 3 && args[3].equals("--speech")){
     	LiveMode lm = new LiveMode();
     	dialogue = lm.getWords();
     } else {
+      System.out.println("Ready");
 	    Scanner sc = new Scanner(System.in);
 	    sc.useDelimiter("[^a-zA-Z]");
 	    while (sc.hasNext()) {
@@ -40,7 +50,7 @@ public final class Main {
 	    }
 	    sc.close();
     }
-    
+    System.out.println("\nLooking for songs...");
     List<Song> res = sm.match(dialogue, 5);
     for (Song s : res) {
       System.out.println(s.getTitle());
