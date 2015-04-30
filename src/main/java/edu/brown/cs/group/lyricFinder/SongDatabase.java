981f1ba61cffb57c88c54e2946bccb1b95a2b1e5
Carlos Gomez <carlos_gomez@brown.edu>
2015-04-30 16:46:30 -0400

    Cleaned up the lyricFinder package a bit

--- src/main/java/edu/brown/cs/group/lyricFinder/SongDatabase.java
+++ src/main/java/edu/brown/cs/group/lyricFinder/SongDatabase.java
@@ -17,42 +17,6 @@ public class SongDatabase {
   private Connection conn;
   private Map<Integer, Song> idToSongMap;
   
-  /*
-  // Just for testing stuff
-  public static void main(String args[]) {
-    String testString = "Pink Floyd - See Emily Play Lyrics | SongMeanings";
-    
-    String[] test1 = testString.split(" - ");
-    System.out.println("test1");
-    for (String s : test1) {
-      System.out.println(s);
-    }
-    System.out.println();
-    
-    String[] test2 = testString.split(" Lyrics | SongMeanings");
-    System.out.println("test2");
-    for (String s : test2) {
-      System.out.println(s);
-    }
-    System.out.println();
-    
-    String replacement = testString.replace(" Lyrics | SongMeanings", "");
-    String[] test3 = replacement.split(" - ");
-    System.out.println("test3");
-    for (String s : test3) {
-      System.out.println(s);
-    }
-    
-    String testLyrics = "What I've kept with me And what I've thrown away And where the hell I've ended up On this glory random day Were the things I really cared about Just left along the way For being too pent up and proud Woke up way too late Feeling hung over and old And the sun was shining bright And I walked barefoot down the road Started thing about my old man It seems that all men Want to get into a car and go anywhere? Here I stand, Sad and free I can't cry and I can't see What I've done God, what have I done So don't you know I'm numb, man No I don't feel a thing at all Cause its all smiles and business these days And I am indifferent to the loss I've faith that there's a soul Whose leading me around I wonder if she knows Which way is down I poured my heart out I poured my heart out It evaporated, see? Blind man on a canyon's edge Of a Panoramic scene Or maybe I'm a kite That's flying high & random Dangling a string Or slumped over in a vacant room Head on a stranger's knee I'm sure back home They think I've lost my mind.";
-    String words = testLyrics.replaceAll("[^a-zA-Z ]", "").toLowerCase();
-    System.out.println(words);
-    String[] wordsSplit = words.split("\\s+");
-    for (String s : wordsSplit) {
-      System.out.println(s);
-    }
-  }
-  */
-  
   public SongDatabase(String db, int songsToGetIfRebuild) throws ClassNotFoundException {
     Class.forName("org.sqlite.JDBC");
     String urlToDB = "jdbc:sqlite:" + db;
@@ -64,13 +28,9 @@ public class SongDatabase {
         buildDatabase(songsToGetIfRebuild);
       }
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      System.out.println(e.getErrorCode());
-      System.out.println(e.getSQLState());
-      e.printStackTrace();
+      System.err.println("ERROR: SQLException when connecting to database or while building it.");
     } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      System.err.println("ERROR: InterruptedException while building database.");
     }
 
     this.idToSongMap = new HashMap<>();
@@ -88,8 +48,7 @@ public class SongDatabase {
       }
       return true;
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      System.err.println("ERROR: SQLException when checking if database should be built.");
       return false;
     }
   }
@@ -118,7 +77,7 @@ public class SongDatabase {
     }
     long endTime = System.currentTimeMillis();
     long totalTime = endTime - startTime;
-    System.out.println("Database built. Took " + totalTime + " ms");
+    System.out.println("Database built. Took " + totalTime + " milliseconds");
   }
 
   private void buildTable(String schema) throws SQLException {
@@ -157,8 +116,7 @@ public class SongDatabase {
       idToSongMap.put(id, song);
       return song;
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      System.err.println("ERROR: SQLException when getting song with ID " + id + ".");
       return null;
     }
   }
@@ -191,8 +149,7 @@ public class SongDatabase {
 
       return songs;
     } catch (SQLException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      System.err.println("ERROR: SQLException when getting all songs.");
       return null;
     }
   }
