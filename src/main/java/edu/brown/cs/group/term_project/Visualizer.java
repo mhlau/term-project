aca716c79fa8486e5fdd1e683f7998a37e156db5
Alexander Levine <alexander_levine@brown.edu>
2015-05-11 02:48:45 -0400

    more checkstyle...

--- src/main/java/edu/brown/cs/group/term_project/Visualizer.java
+++ src/main/java/edu/brown/cs/group/term_project/Visualizer.java
@@ -1,37 +1,35 @@
 package edu.brown.cs.group.term_project;
-import java.io.IOException;
-import java.io.File;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.List;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 //import java.nio.file.Files;
 
 
-public class Visualizer implements AutoCloseable{
+public class Visualizer implements AutoCloseable {
   private List<BufferedReader> s;
   private List<Process> p;
-  int lastWord;
+  private int lastWord;
   public Visualizer() throws IOException {
     s = new ArrayList<BufferedReader>();
     p =  new ArrayList<Process>();
     String[] getSourcesCommand = {
-        "/bin/sh", "-c",
-        "pacmd list |grep \".monitor>\" | sed \'s/.*name: <//\' |"
+      "/bin/sh", "-c",
+      "pacmd list |grep \".monitor>\" | sed \'s/.*name: <//\' |"
         + "sed \'s/>[^>]*//'"
     };
-    
     Process sourcesP = Runtime.getRuntime().exec(getSourcesCommand);
     Scanner sbr = new Scanner(sourcesP.getInputStream());
     while (sbr.hasNextLine()) {
       String[] cmd = {
-          "/bin/sh", "-c",
-          "pacat --record -d"
-              + sbr.nextLine()
-              + "|od | cut -d \' \' -f 3"
-              + "| stdbuf -i0 -o0 -e0 sed -u -n \'0~400p\'"
+        "/bin/sh", "-c",
+        "pacat --record -d"
+          + sbr.nextLine()
+          + "|od | cut -d \' \' -f 3"
+          + "| stdbuf -i0 -o0 -e0 sed -u -n \'0~400p\'"
       };
       Process pr = Runtime.getRuntime().exec(cmd);
       BufferedReader sr = new BufferedReader(new InputStreamReader(
@@ -39,10 +37,11 @@ public class Visualizer implements AutoCloseable{
       s.add(sr);
       p.add(pr);
     }
-    
 
     lastWord = 0;
   }
+  private static final int BASE = 8;
+  private static final int SHORT_SIGN_CONSTANT = 65536;  
   public synchronized int getWord() {
     try {
       int tot = 0;
@@ -52,12 +51,14 @@ public class Visualizer implements AutoCloseable{
           count++;
           String snext = sr.readLine();
           System.out.println(snext);
-          int i = Integer.parseInt(snext, 8);
-          tot += Math.abs((i > Short.MAX_VALUE ? i-65536: i)) ;
+          int i = Integer.parseInt(snext, BASE);
+          tot += Math.abs(
+            (i > Short.MAX_VALUE ? i - SHORT_SIGN_CONSTANT : i));
         }
       }
-      lastWord = tot/count;
-    } catch (Exception e) {
+      lastWord = tot / count;
+    } catch (IOException e) {
+      //ioException
     }
     return lastWord;
   }
@@ -67,7 +68,6 @@ public class Visualizer implements AutoCloseable{
     }
     for (BufferedReader sr : s) {
       sr.close();
-    }    
+    }
   }
-  
 }
